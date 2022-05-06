a9adf8d2a70259609cf96c92433e3f04723a969c
MisterMikeMC <diegoerr04@gmail.com>
2022-05-06 01:12:10 -0500

    :sparkles: Header was added.

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 export const Header = () => {
+  const [show, setShow] = useState(false);
   return (
-    <header className="PageHeader">
-      <ul>
-        <li>1</li>
-      </ul>
+    <header
+      className={show === false ? "PageHeader" : "PageHeaderColumsActivate"}
+    >
+      <div
+        className={show === false ? "GridTwoColums" : "GridTwoColumsActivate"}
+      >
+        <div className={show === false ? "GridOne" : "GridOneActivate"}>
+          <img
+            onClick={() => setShow(!show)}
+            className="ImagenLogo"
+            src="https://cdn.discordapp.com/attachments/889575240617852989/972005800174297118/MrMikeDev.png"
+            alt="Logo"
+          />
+        </div>
+        <div className={show === false ? "GridTwo" : "GridTwoActivate"}>
+          <ul className="List">
+            <li className={show === false ? "ListItem" : "ListItemActivate"}>
+              <Link className="HeaderLink" to="/">
+                Home
+              </Link>
+            </li>
+            <li className={show === false ? "ListItem" : "ListItemActivate"}>
+              <Link className="HeaderLink" to="/projects">
+                Projects
+              </Link>
+            </li>
+            <li className={show === false ? "ListItem" : "ListItemActivate"}>
+              <Link className="HeaderLink" to="/skills">
+                Skills
+              </Link>
+            </li>
+            <li className={show === false ? "ListItem" : "ListItemActivate"}>
+              <Link className="HeaderLink" to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 };
